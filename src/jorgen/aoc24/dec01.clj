ec705bd6387e86eeb98386111ce946e2410bb3b2
Jörgen Lundberg <jorgen.lundberg@gmail.com>
2024-12-01 15:14:28 +0100

    Dec 01 - part 2

--- src/jorgen/aoc24/dec01.clj
+++ src/jorgen/aoc24/dec01.clj
@@ -3,11 +3,13 @@
     [clojure.string :as str]
     [jorgen.util :as util]))
 
+
 (defn location-ids [lines]
   (->> lines
        (map (fn [s] (str/split s #"\s+")))
        (map (fn [[a b]] [(util/parse-int a) (util/parse-int b)]))
        util/transpose))
+
 
 (defn total-distance [locations]
   (->> (map sort locations)
@@ -17,14 +19,34 @@
        (map abs)
        (reduce +)))
 
+
+(defn occurances [xs]
+  (reduce (fn [acc x]
+            (update acc x (fnil inc 0)))
+          {}
+          xs))
+
+
+(defn proximity-score [ids hist]
+  (reduce (fn [acc x]
+            (+ acc (* x (get hist x 0))))
+          0
+          ids))
+
+
 (defn part1 [lines]
   (->> (location-ids lines)
        (total-distance)))
 
+
+(defn part2 [lines]
+  (let [[ids1 ids2] (location-ids lines)
+        hist (occurances ids2)]
+    (proximity-score ids1 hist)))
+
+
 (comment
   (time (part1 (util/file->lines "dec01_sample.txt")))
-  (time (part1 (util/file->lines "dec01_input.txt"))))
-;(time (part2 (util/file->lines "dec01_sample.txt")))
-;(time (part2 (util/file->lines "dec01_input.txt"))))
-
-
+  (time (part1 (util/file->lines "dec01_input.txt")))
+  (time (part2 (util/file->lines "dec01_sample.txt")))
+  (time (part2 (util/file->lines "dec01_input.txt"))))
--- test/jorgen/aoc24/dec01_test.clj
+++ test/jorgen/aoc24/dec01_test.clj
@@ -1,16 +1,16 @@
 (ns jorgen.aoc24.dec01-test
   (:require
     [clojure.test :refer :all]
-    [jorgen.aoc24.util :as util]
+    [jorgen.util :as util]
     [jorgen.aoc24.dec01 :refer :all]))
 
 
 (deftest testing-
-  (testing "It should "
-    (is (= 1 (part1 (util/file->lines "dec01_sample.txt"))))
-    (is (= 1 (part1 (util/file->lines "dec01_input.txt"))))))
+  (testing "It should calculate the total distance"
+    (is (= 11 (part1 (util/file->lines "dec01_sample.txt"))))
+    (is (= 2285373 (part1 (util/file->lines "dec01_input.txt")))))
 
 
-  ;(testing "It should "
-  ;  (is (= 1 (part (util/file->lines "dec01_sample.txt"))))
-  ;  (is (= 1 (part2 (util/file->lines "dec01_input.txt"))))))
+  (testing "It should calculate the proximity score"
+    (is (= 31 (part2 (util/file->lines "dec01_sample.txt"))))
+    (is (= 21142653 (part2 (util/file->lines "dec01_input.txt"))))))
