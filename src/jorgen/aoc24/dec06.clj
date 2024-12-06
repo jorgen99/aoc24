0a5fe67e8d70a361538dd673439c951310d2bb36
Jörgen Lundberg <jorgen.lundberg@evinova.com>
2024-12-06 21:28:35 +0100

    WIP

--- src/jorgen/aoc24/dec06.clj
+++ src/jorgen/aoc24/dec06.clj
@@ -3,7 +3,7 @@
     [clojure.string :as str]
     [jorgen.util :as util]))
 
-(def direction
+(def char->direction
   {\^ :n
    \> :e
    \V :s
@@ -16,18 +16,37 @@
    :w :n})
 
 (defn leffe [char line]
-  [(get direction char)
+  [(get char->direction char)
    (first (util/indicies-of-char char line))])
 
 
 (defn dir-and-pos [line-no line]
-  (->> (keys direction)
+  (->> (keys char->direction)
        (map #(leffe % line))
        (remove #(nil? (second %)))
        (map (fn [[dir col]] [dir [col line-no]]))
        (first)))
 
-(let [lines (util/file->lines "dec06_input.txt")
+
+(defn walk [starting-pos direction grid]
+  (loop [steps []
+         dir direction
+         current-pos starting-pos
+         visited #{}]
+    (let [_ (prn "-----------------")
+          _ (prn "dir" dir)
+          _ (prn "current-pos" current-pos)
+          _ (prn "steps" steps)
+          char-at-next-step (util/value-in-grid grid (util/take-step current-pos dir))
+          _ (prn "char-at-next-step" char-at-next-step)
+          loop? (contains? visited current-pos)]
+      (cond
+        (nil? char-at-next-step) (conj steps [dir current-pos])
+        (= char-at-next-step "#") (recur steps (clockwise dir) current-pos)
+        :else (recur (conj steps [dir current-pos]) dir (util/take-step  current-pos dir))))))
+
+
+(let [lines (util/file->lines "dec06_sample.txt")
       grid (util/parse-grid-of-chars lines)
       [dir pos] (->> lines
                      (map #(dir-and-pos 6 %))
@@ -38,21 +57,13 @@
               (let [[dir pos] (dir-and-pos line-no (first remaining-lines))]
                 (if dir
                   [dir pos]
-                  (recur (rest remaining-lines) (inc line-no)))))
-      ss (loop [steps []
-                dir d
-                current-pos p]
-           (let [;_ (prn "-----------------")
-                 ;_ (prn "dir" dir)
-                 ;_ (prn "current-pos" current-pos)
-                 ;M_ (prn "steps" steps)
-                 char-at-next-step (util/value-in-grid grid (util/take-step current-pos dir))]
-                 ;_ (prn "char-at-next-step" char-at-next-step)]
-             (cond
-               (nil? char-at-next-step) (conj steps current-pos)
-               (= char-at-next-step "#") (recur steps (clockwise dir) current-pos)
-               :else (recur (conj steps current-pos) dir (util/take-step  current-pos dir)))))]
- (count (distinct ss)))
+                  (recur (rest remaining-lines) (inc line-no)))))]
+      ;ss (walk p d grid)]
+ (->> (walk p d grid)
+      (map second)
+      distinct
+      count))
+
 
 
 
